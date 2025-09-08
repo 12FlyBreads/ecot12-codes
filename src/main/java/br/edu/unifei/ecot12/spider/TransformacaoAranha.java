70c0230e268846625cda0862daf67045874f0e27
unknown <arturgsimao@gmail.com>
2025-09-08 16:31:26 -0300

    lab05: spider-class

---
+++ src/main/java/br/edu/unifei/ecot12/spider/TransformacaoAranha.java
@@ -0,0 +1,26 @@
+package br.edu.unifei.ecot12.spider;
+
+import java.util.Date;
+
+public class TransformacaoAranha {
+    public Evento picada(String nome, String codinome){
+        Fisico f1 = new Fisico();
+        f1.setNome("andar pelas paredes");
+        f1.setPeso(1000);
+        Fisico f2 = new Fisico();
+        f2.setNome("superforca");
+        f2.setPeso(01000);
+        Aranha a = new Aranha();
+        a.setCodinome(codinome);
+        a.getPoderes().add(f1);
+        a.getPoderes().add(f2);
+        Humano h = new Humano();
+        h.setNome(nome);
+        h.setTotem(a);
+        Evento e = new Evento();
+        e.setDescricao("Transformacao");
+        e.setData(new Date());
+        e.getSeres().add(h);
+        return null;
+    }
+}
